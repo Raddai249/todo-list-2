8b09f227d0aa03bc3e6b6b745c8efacba23f703e
Raddai249 <raddaikabongo7@gmail.com>
2025-12-31 21:09:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function addTask(event) {
-    event.preventDefault(); // empêche le rechargement
+const addBtn = document.getElementById("addBtn");
+const input = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
 
-    const input = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
+addBtn.addEventListener("click", function () {
+    const task = input.value.trim();
 
-    if (input.value.trim() === "") {
+    if (task === "") {
         alert("Écris une tâche !");
         return;
     }
 
     const li = document.createElement("li");
-    li.textContent = input.value;
+    li.textContent = task;
 
     taskList.appendChild(li);
     input.value = "";
-}
-
+});
 
